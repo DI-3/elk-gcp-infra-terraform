4f710f54c71feabea65079715e75cf61e4e10a55
Mithun <mitun311@gmail.com>
2023-03-11 15:21:20 -0500

    Added the Initial Infra For a GKE Cluster

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+    type = string
+    description = "The Google Cloud Project Id"
+}
+
+variable "region" {
+    type = string
+    default = "us-central1"
+}
+
+
+variable "project_id" {
+  type        = string
+  description = "The Google Cloud Project Id"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
