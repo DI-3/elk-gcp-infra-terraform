747e3418acb99899a56c2eb8e6466e2dc76a7d69
Mithun <mitun311@gmail.com>
2023-03-12 11:10:50 -0400

    Add the Subnet in the GCP Reunion project

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource "google_container_cluster" "primary" {
     remove_default_node_pool = true
     initial_node_count = 1 
 
-    network = google_compute_network.vpc.name
-    subnetwork = google_compute_subnetwork.subnet.name
+    network = "universal-valve-bucket-373803-vpc"
+    subnetwork = "universal-valve-bucket-373803-vpc-subnet"
 }
 
 
