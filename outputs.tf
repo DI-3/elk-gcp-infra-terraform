6fb8670ab6aa64e24a63f06c742cc4d8f1e434e3
Mithun <mitun311@gmail.com>
2023-03-12 19:09:08 -0400

    Adding node pools remove output vats

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,12 @@ output "project_id" {
     description = "GCloud Project ID"
 }
 
-output "kubernetes_cluster_name" {
-    value = google_container_cluster.primary.name
-    description = "GKE Cluster Name"
-}
+# output "kubernetes_cluster_name" {
+#     value = google_container_cluster.primary.name
+#     description = "GKE Cluster Name"
+# }
 
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.primary.endpoint
-  description = "GKE Cluster Host"
-}
+# output "kubernetes_cluster_host" {
+#   value       = google_container_cluster.primary.endpoint
+#   description = "GKE Cluster Host"
+# }
